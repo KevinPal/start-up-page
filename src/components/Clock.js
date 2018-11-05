58f5bddf4054fc2ae05bdc8ea22f9ebd84d7eea2
coffeevector <coffeevector@gmail.com>
2018-11-04 16:05:05 -0800

    fixed single digit display bug

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Clock extends Component {
 	render() {
 		var d = new Date();
+		var hour = "" + d.getHours();
+		if (hour.length === 1){
+			hour = "0" + hour;	
+		}
+		var minute = "" + d.getMinutes();
+		if (minute.length === 1){
+			minute = "0" + minute;
+		}
+		var display = hour + " " + minute;
 		return (
 			<div className="Clock">
-				{d.getHours()} {d.getMinutes()}
+				{display}
 			</div>
 		);
 	}
