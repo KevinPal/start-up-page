d5e7b1e276a0ca88d315795257aee8f87ff9b885
coffeevector <coffeevector@gmail.com>
2018-11-07 19:34:54 -0800

    Colorized my schedule

--- src/components/Information.js
+++ src/components/Information.js
@@ -8,42 +8,42 @@ class Information extends Component {
 	render() {
 		return (
 			<table class="table table-bordered table-hover table-condensed">
-<thead><tr>
-<th title="Field #3">Monday</th>
-<th title="Field #4">Tuesday</th>
-<th title="Field #5">Wednesday</th>
-<th title="Field #6">Thursday</th>
-<th title="Field #7">Friday</th>
-</tr></thead>
-<tbody><tr>
-<td>9:30 AM College Reading and Composition 11:20 AM</td>
-<td>9:30 AM Measuring our Physical World 11:20 AM</td>
-<td>9:30 AM College Reading and Composition 11:20 AM</td>
-<td>9:30 AM Measuring our Physical World 11:20 AM</td>
-<td> </td>
-</tr>
-<tr>
-<td> </td>
-<td> </td>
-<td> </td>
-<td> </td>
-<td>10:30 AM Data Structures: Lab 1:20 PM</td>
-</tr>
-<tr>
-<td>1:30 PM Physics II: Lab 3:20 PM</td>
-<td>1:30 PM Physics II 2:45 PM</td>
-<td>1:30 PM Physics II: Disc 3:20 PM</td>
-<td>1:30 PM Physics II 2:45 PM</td>
-<td> </td>
-</tr>
-<tr>
-<td>4:30 PM Data Structures 5:45 PM</td>
-<td> </td>
-<td>4:30 PM Data Structures 5:45 PM</td>
-<td> </td>
-<td> </td>
-</tr>
-</tbody></table>
+				<thead>
+					<tr>
+						<th className="Day">Monday</th>
+						<th className="Day">Tuesday</th>
+						<th className="Day">Wednesday</th>
+						<th className="Day">Thursday</th>
+						<th className="Day">Friday</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td className="CRC">9:30 AM College Reading and Composition 11:20 AM</td>
+						<td className="MPW">9:30 AM Measuring our Physical World 11:20 AM</td>
+						<td className="CRC">9:30 AM College Reading and Composition 11:20 AM</td>
+						<td className="MPW">9:30 AM Measuring our Physical World 11:20 AM</td>
+					</tr>
+					<tr>
+					<td> </td>
+					<td> </td>
+					<td> </td>
+					<td> </td>
+						<td className="DSL">10:30 AM Data Structures: Lab 1:20 PM</td>
+					</tr>
+					<tr>
+						<td className="PL">1:30 PM Physics II: Lab 3:20 PM</td>
+						<td className="P">1:30 PM Physics II 2:45 PM</td>
+						<td className="PD">1:30 PM Physics II: Disc 3:20 PM</td>
+						<td className="P">1:30 PM Physics II 2:45 PM</td>
+					</tr>
+					<tr>
+						<td className="DS">4:30 PM Data Structures 5:45 PM</td>
+						<td> </td>
+						<td className="DS">4:30 PM Data Structures 5:45 PM</td>
+					</tr>
+				</tbody>
+			</table>
 		);
 	}
 }
