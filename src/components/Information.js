c850d039d9b7371f0657ac1d0217abf39d65f8d1
coffeevector <coffeevector@gmail.com>
2018-11-07 19:05:54 -0800

    Added real schedule functionality

--- src/components/Information.js
+++ src/components/Information.js
@@ -7,23 +7,43 @@ class Information extends Component {
 	// just don't keep your tab open between morning and afternoon
 	render() {
 		return (
-			<table className="Information">
-				<tr>
-					<th>Monday</th>
-					<th>Tuesday</th>
-					<th>Wednesday</th>
-					<th>Thursday</th>
-					<th>Friday</th>
-					<th>Saturday</th>
-					<th>Sunday</th>
-				</tr>
-				<tr>
-					<td>Writing </td>
-					<td>Spark</td>
-					<td>Writing</td>
-					<td>Spark</td>
-				</tr>
-			</table>
+			<table class="table table-bordered table-hover table-condensed">
+<thead><tr>
+<th title="Field #3">Monday</th>
+<th title="Field #4">Tuesday</th>
+<th title="Field #5">Wednesday</th>
+<th title="Field #6">Thursday</th>
+<th title="Field #7">Friday</th>
+</tr></thead>
+<tbody><tr>
+<td>9:30 AM College Reading and Composition 11:20 AM</td>
+<td>9:30 AM Measuring our Physical World 11:20 AM</td>
+<td>9:30 AM College Reading and Composition 11:20 AM</td>
+<td>9:30 AM Measuring our Physical World 11:20 AM</td>
+<td> </td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+<td> </td>
+<td> </td>
+<td>10:30 AM Data Structures: Lab 1:20 PM</td>
+</tr>
+<tr>
+<td>1:30 PM Physics II: Lab 3:20 PM</td>
+<td>1:30 PM Physics II 2:45 PM</td>
+<td>1:30 PM Physics II: Disc 3:20 PM</td>
+<td>1:30 PM Physics II 2:45 PM</td>
+<td> </td>
+</tr>
+<tr>
+<td>4:30 PM Data Structures 5:45 PM</td>
+<td> </td>
+<td>4:30 PM Data Structures 5:45 PM</td>
+<td> </td>
+<td> </td>
+</tr>
+</tbody></table>
 		);
 	}
 }
