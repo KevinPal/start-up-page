3af2065b9c777de9e55737db99271ec8ff2f4bd3
coffeevector <coffeevector@gmail.com>
2018-11-07 21:22:09 -0800

    Aesthetic Changes for Spring Schedule

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -51,7 +51,7 @@ class Schedule extends Component {
 			<tbody>
 				<tr>
 					<td className="Yellow"><big>8:30 AM</big><br/>Intro to East Asia<br/><big>9:20 AM</big></td>
-					<td className="Green"><big>8:30 AM</big><br/>Biology Today Discussion<br/><big>10:20 AM</big></td>
+					<td className="Red"><big>8:30 AM</big><br/>Biology Today Discussion<br/><big>10:20 AM</big></td>
 					<td className="Yellow"><big>8:30 AM</big><br/>Intro to East Asia<br/><big>9:20 AM</big></td>
 					<td></td>
 					<td className="Yellow"><big>8:30 AM</big><br/>Intro to East Asia<br/><big>9:20 AM</big></td>
@@ -59,7 +59,7 @@ class Schedule extends Component {
 				<tr>
 					<td></td>
 					<td></td>
-					<td className="Green"><big>10:30 AM</big><br/>Intro to East Asia Discussion<br/><big>11:20 AM</big></td>
+					<td className="Yellow"><big>10:30 AM</big><br/>Intro to East Asia Discussion<br/><big>11:20 AM</big></td>
 					<td></td>
 				</tr>
 				<tr>
@@ -71,15 +71,15 @@ class Schedule extends Component {
 				</tr>
 				<tr>
 					<td className="Magenta"><big>4:30 PM</big><br/>Discrete Mathematics<br/><big>5:45 PM</big></td>
-					<td className="Red"><big>1:30 PM</big><br/>Discrete Mathematics Lab<br/><big>4:20 PM</big></td>
+					<td className="Magenta"><big>1:30 PM</big><br/>Discrete Mathematics Lab<br/><big>4:20 PM</big></td>
 					<td className="Magenta"><big>4:30 PM</big><br/>Discrete Mathematics<br/><big>5:45 PM</big></td>
 					<td></td>
 					<td className="Green"><big>2:30 PM</big><br/>Introduction To Philisophy Discussion<br/><big>3:20 PM</big></td>
 				</tr>
 				<tr>
-					<td className="White"><big>7:30 PM</big><br/>Introduction To Philosophy<br/><big>8:45 PM</big></td>
-					<td> </td>
-					<td className="White"><big>7:30 PM</big><br/>Introduction To Philosophy<br/><big>8:45 PM</big></td>
+					<td className="Green"><big>7:30 PM</big><br/>Introduction To Philosophy<br/><big>8:45 PM</big></td>
+					<td></td>
+					<td className="Green"><big>7:30 PM</big><br/>Introduction To Philosophy<br/><big>8:45 PM</big></td>
 				</tr>
 			</tbody>
 		</table>
