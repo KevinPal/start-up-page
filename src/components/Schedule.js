33b331d4ac2930a970e65198631d04ab7f9e9b8a
coffeevector <coffeevector@gmail.com>
2018-11-07 19:41:43 -0800

    Moved table to fall jsx variable

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -3,48 +3,48 @@ import React, {
 } from 'react';
 
 class Information extends Component {
-	// this will only render once and won't update for the morning or afternoon
-	// just don't keep your tab open between morning and afternoon
+	fall = (
+		<table className="Table">
+			<thead>
+				<tr>
+					<th className="Day">Monday</th>
+					<th className="Day">Tuesday</th>
+					<th className="Day">Wednesday</th>
+					<th className="Day">Thursday</th>
+					<th className="Day">Friday</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td className="CRC">9:30 AM College Reading and Composition 11:20 AM</td>
+					<td className="MPW">9:30 AM Measuring our Physical World 11:20 AM</td>
+					<td className="CRC">9:30 AM College Reading and Composition 11:20 AM</td>
+					<td className="MPW">9:30 AM Measuring our Physical World 11:20 AM</td>
+				</tr>
+				<tr>
+				<td> </td>
+				<td> </td>
+				<td> </td>
+				<td> </td>
+					<td className="DSL">10:30 AM Data Structures: Lab 1:20 PM</td>
+				</tr>
+				<tr>
+					<td className="PL">1:30 PM Physics II: Lab 3:20 PM</td>
+					<td className="P">1:30 PM Physics II 2:45 PM</td>
+					<td className="PD">1:30 PM Physics II: Disc 3:20 PM</td>
+					<td className="P">1:30 PM Physics II 2:45 PM</td>
+				</tr>
+				<tr>
+					<td className="DS">4:30 PM Data Structures 5:45 PM</td>
+					<td> </td>
+					<td className="DS">4:30 PM Data Structures 5:45 PM</td>
+				</tr>
+			</tbody>
+		</table>
+	);
+
 	render() {
-		return (
-			<table class="table table-bordered table-hover table-condensed">
-				<thead>
-					<tr>
-						<th className="Day">Monday</th>
-						<th className="Day">Tuesday</th>
-						<th className="Day">Wednesday</th>
-						<th className="Day">Thursday</th>
-						<th className="Day">Friday</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td className="CRC">9:30 AM College Reading and Composition 11:20 AM</td>
-						<td className="MPW">9:30 AM Measuring our Physical World 11:20 AM</td>
-						<td className="CRC">9:30 AM College Reading and Composition 11:20 AM</td>
-						<td className="MPW">9:30 AM Measuring our Physical World 11:20 AM</td>
-					</tr>
-					<tr>
-					<td> </td>
-					<td> </td>
-					<td> </td>
-					<td> </td>
-						<td className="DSL">10:30 AM Data Structures: Lab 1:20 PM</td>
-					</tr>
-					<tr>
-						<td className="PL">1:30 PM Physics II: Lab 3:20 PM</td>
-						<td className="P">1:30 PM Physics II 2:45 PM</td>
-						<td className="PD">1:30 PM Physics II: Disc 3:20 PM</td>
-						<td className="P">1:30 PM Physics II 2:45 PM</td>
-					</tr>
-					<tr>
-						<td className="DS">4:30 PM Data Structures 5:45 PM</td>
-						<td> </td>
-						<td className="DS">4:30 PM Data Structures 5:45 PM</td>
-					</tr>
-				</tbody>
-			</table>
-		);
+		return this.fall;
 	}
 }
 
